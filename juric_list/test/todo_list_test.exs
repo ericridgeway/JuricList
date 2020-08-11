a974025975e30e4c03d6093ffd8d927cc54dad1a
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-11 12:24:19 -0400

    Extract setup block

--- juric_list/test/todo_list_test.exs
+++ juric_list/test/todo_list_test.exs
@@ -3,16 +3,22 @@ defmodule JuricListTest.TodoList do
 
   alias JuricList.{TodoList}
 
-  test "new, add_entry, and titles" do
+  setup do
     todo_list =
       TodoList.new()
       |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Dentist"})
       |> TodoList.add_entry(%{date: ~D[2018-12-20], title: "Shopping"})
       |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Movies"})
 
-    IO.puts ""; require InspectVars; InspectVars.inspect([todo_list])
+    {:ok, todo_list: todo_list}
+  end
+
+  test "new, add_entry, and titles", %{todo_list: todo_list} do
     assert TodoList.titles(todo_list, ~D[2018-12-20]) == ["Shopping"]
     assert TodoList.titles(todo_list, ~D[2018-12-19]) == ["Dentist", "Movies"]
     assert TodoList.titles(todo_list, ~D[2018-01-01]) == []
+  end
+
+  test "update_entry" do
   end
 end
