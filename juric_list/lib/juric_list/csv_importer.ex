a7f77ece8599562177ff6726640233200d61b4ee
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-11 22:10:47 -0400

    Finish import pipeine

--- juric_list/lib/juric_list/csv_importer.ex
+++ juric_list/lib/juric_list/csv_importer.ex
@@ -1,11 +1,27 @@
 defmodule JuricList.CsvImporter do
+  import ShorterMaps
+
+  alias JuricList.{TodoList}
 
   def import(file) do
     file
     |> File.stream!()
     |> Enum.map(&String.trim/1)
-    |> IO.inspect(label: "1")
-    # |> Enum.map(fn a -> a end)
-    # |> IO.inspect(label: "asd")
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(&List.to_tuple/1)
+    |> Enum.map(&make_entry/1)
+    |> TodoList.new()
+  end
+
+
+  defp make_entry({date_string, title}) do
+    {yyyy, mm, dd} =
+      date_string
+      |> String.split("/")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+
+    {:ok, date} = Date.new(yyyy, mm, dd)
+    ~M{date, title}
   end
 end
