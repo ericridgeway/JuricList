7fc7ac09a6451273dfc1a0e159137090dfe3410e
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-12 23:03:41 -0400

    Setup server placeholder funs and initial tests

--- juric_list/lib/juric_list/todo_server.ex
+++ juric_list/lib/juric_list/todo_server.ex
@@ -1,3 +1,18 @@
 defmodule JuricList.TodoServer do
 
+  def start() do
+    -1
+  end
+
+  def add_entry(_, _) do
+    :ok
+  end
+
+  def titles(_, _) do
+    -1
+  end
+
+  def finish(_) do
+    -1
+  end
 end
--- juric_list/test/todo_server_test.exs
+++ juric_list/test/todo_server_test.exs
@@ -1,5 +1,27 @@
 defmodule JuricListTest.TodoServer do
   use ExUnit.Case
+  import ShorterMaps
 
   alias JuricList.{TodoServer}
+
+  @date1 ~D[2018-12-19]
+  @date2 ~D[2018-12-20]
+
+  setup do
+    todo_server = TodoServer.start()
+
+    on_exit fn ->
+      TodoServer.finish(todo_server)
+    end
+
+    ~M{todo_server}
+  end
+
+  test "TODO", ~M{todo_server} do
+    :ok = TodoServer.add_entry(todo_server, %{date: @date1, title: "Dentist"})
+    :ok = TodoServer.add_entry(todo_server, %{date: @date2, title: "Shopping"})
+    :ok = TodoServer.add_entry(todo_server, %{date: @date1, title: "Movies"})
+
+    # assert TodoServer.titles(todo_server, @date1) == ["Dentist", "Movies"]
+  end
 end
