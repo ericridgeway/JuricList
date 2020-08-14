7c5cf66eea55945e41d17e72a168b99269853765
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-13 21:12:37 -0400

    Working custom generic server process

--- juric_list/lib/juric_list/key_value_store.ex
+++ juric_list/lib/juric_list/key_value_store.ex
@@ -1,3 +1,14 @@
 defmodule JuricList.KeyValueStore do
 
+  def init() do
+    %{}
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {Map.get(state, key), state}
+  end
 end
--- juric_list/lib/juric_list/server_process.ex
+++ juric_list/lib/juric_list/server_process.ex
@@ -1,3 +1,40 @@
 defmodule JuricList.ServerProcess do
 
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  def call(pid, request) do
+    send(pid, {request, self()})
+
+    receive do
+      {:response, response} ->
+        response
+
+    # after
+    #   5000 -> {:error, :timeout}
+    end
+  end
+
+
+  defp loop(callback_module, state) do
+    receive do
+      {request, caller} ->
+        {response, new_state} =
+          callback_module.handle_call(request, state)
+
+        send(caller, {:response, response})
+
+        loop(callback_module, new_state)
+
+      _ ->
+        # TODO DRY and double check
+        IO.puts "incorrect response format"
+
+        loop(callback_module, state)
+    end
+  end
 end
