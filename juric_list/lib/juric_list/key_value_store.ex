8afa28cfc28da1591c5deabc56fe7481fe564b06
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-14 12:36:14 -0400

    Convert KeyValueStore to use real genserver

--- juric_list/lib/juric_list/key_value_store.ex
+++ juric_list/lib/juric_list/key_value_store.ex
@@ -1,29 +1,38 @@
 defmodule JuricList.KeyValueStore do
+  use GenServer
 
   alias JuricList.{ServerProcess, KeyValueStore}
 
+  #
+  # Client
+  #
+
   def start() do
-    ServerProcess.start(KeyValueStore)
+    GenServer.start(KeyValueStore, nil)
   end
 
   def put(pid, key, value) do
-    ServerProcess.cast(pid, {:put, key, value})
+    GenServer.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
-    ServerProcess.call(pid, {:get, key})
+    GenServer.call(pid, {:get, key})
   end
 
 
-  def init() do
-    %{}
+  #
+  # Server
+  #
+
+  def init(_) do
+    {:ok, %{}}
   end
 
   def handle_cast({:put, key, value}, state) do
-    Map.put(state, key, value)
+    {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_call({:get, key}, state) do
-    {Map.get(state, key), state}
+  def handle_call({:get, key}, _from, state) do
+    {:reply, Map.get(state, key), state}
   end
 end
