aae978c04cf2b434caaba28b28494edff2c09df2
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 16:53:10 -0400

    Pull logic out of server and down into impl where it belongs

--- cache/lib/cache/impl.ex
+++ cache/lib/cache/impl.ex
@@ -3,5 +3,18 @@ defmodule Cache.Impl do
   defdelegate new(), to: Map
   defdelegate fetch(map, key), to: Map
   defdelegate put(map, key, value), to: Map
+
+  def get_existing_or_make_new(state, todo_list_name) do
+    case fetch(state, todo_list_name) do
+      {:ok, todo_server} ->
+        {state, todo_server}
+
+      _ ->
+        {:ok, new_server} = TodoServer.start()
+        new_state = put(state, todo_list_name, new_server)
+
+        {new_state, new_server}
+    end
+  end
 end
 
--- cache/lib/cache/server.ex
+++ cache/lib/cache/server.ex
@@ -10,17 +10,9 @@ defmodule Cache.Server do
 
   @impl GenServer
   def handle_call({:server_process, todo_list_name}, _from, state) do
+    {state, todo_server} = Impl.get_existing_or_make_new(state, todo_list_name)
 
-    case Impl.fetch(state, todo_list_name) do
-      {:ok, todo_server} ->
-        {:reply, todo_server, state}
-
-      _ ->
-        {:ok, new_server} = TodoServer.start()
-        new_state = Impl.put(state, todo_list_name, new_server)
-
-        {:reply, new_server, new_state}
-    end
+    {:reply, todo_server, state}
   end
 end
 
