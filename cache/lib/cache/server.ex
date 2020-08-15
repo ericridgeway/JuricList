deb8f92100cc24fabfd999e6de9dd6e4b2201698
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 17:37:25 -0400

    Clean up pipeline needing to "pass multiple parts thru pipeline", ahhh, WITH

--- cache/lib/cache/server.ex
+++ cache/lib/cache/server.ex
@@ -10,9 +10,10 @@ defmodule Cache.Server do
 
   @impl GenServer
   def handle_call({:server_process, todo_list_name}, _from, state) do
-    state = Impl.add_if_doesnt_exist(state, todo_list_name)
-    todo_server = Impl.fetch!(state, todo_list_name)
-
-    {:reply, todo_server, state}
+    with state <- Impl.add_if_doesnt_exist(state, todo_list_name),
+         {:ok, todo_server} <- Impl.fetch(state, todo_list_name)
+    do
+      {:reply, todo_server, state}
+    end
   end
 end
