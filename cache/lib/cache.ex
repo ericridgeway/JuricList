ab2c9107bfe648c904e974e9dafbea18ea2c5c18
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 17:00:36 -0400

    Add beautiful doc stuff to the "export these functions" file which does only that. Interface file or whatever. so sweet

--- cache/lib/cache.ex
+++ cache/lib/cache.ex
@@ -1,9 +1,18 @@
 defmodule Cache do
+  @moduledoc """
+  Run multiple TodoServer's at once, each saved under a Name
+  """
 
   def start() do
     GenServer.start(__MODULE__.Server, nil)
   end
 
+  @doc """
+  Get server process PID for given 'todo_list_name'
+
+  Creates new server_process if one doesn't exist yet for that
+  name
+  """
   def server_process(cache_pid, todo_list_name) do
     GenServer.call(cache_pid, {:server_process, todo_list_name})
   end
