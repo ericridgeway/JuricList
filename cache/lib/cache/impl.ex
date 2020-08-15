0dc4a3350efa76673ddc7efc1b7b512328ecf653
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 17:53:31 -0400

    Kill complicated fetch ok reply tuples

--- cache/lib/cache/impl.ex
+++ cache/lib/cache/impl.ex
@@ -1,7 +1,7 @@
 defmodule Cache.Impl do
 
   defdelegate new(), to: Map
-  defdelegate fetch(map, key), to: Map
+  defdelegate get(map, key), to: Map
 
   def add_if_doesnt_exist(state, todo_list_name) do
     Map.put_new_lazy(state, todo_list_name, fn ->
--- cache/lib/cache/server.ex
+++ cache/lib/cache/server.ex
@@ -11,12 +11,10 @@ defmodule Cache.Server do
   @impl GenServer
   def handle_call({:server_process, todo_list_name}, _from, state) do
     with state <- Impl.add_if_doesnt_exist(state, todo_list_name),
-         {:ok, todo_server} <- Impl.fetch(state, todo_list_name)
+         todo_server <- Impl.get(state, todo_list_name)
     do
       state
       |> reply(todo_server)
-    else
-      error -> reply(state, error)
     end
   end
 
