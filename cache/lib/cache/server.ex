718a875711426ea5e04dda2bbd7e1e4c5909536b
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 17:24:16 -0400

    for revert, not-working pipeline-ification

--- cache/lib/cache/server.ex
+++ cache/lib/cache/server.ex
@@ -10,20 +10,27 @@ defmodule Cache.Server do
 
   @impl GenServer
   def handle_call({:server_process, todo_list_name}, _from, state) do
-    state = Impl.add_if_doesnt_exist(state, todo_list_name)
-    todo_server = Impl.fetch!(state, todo_list_name)
-
-    {:reply, todo_server, state}
+    # state = Impl.add_if_doesnt_exist(state, todo_list_name)
+    # todo_server = Impl.fetch!(state, todo_list_name)
+    state
+    |> Impl.add_if_doesnt_exist(todo_list_name)
+    |> reply_with_process()
   end
+
+  defp reply_with_process(state) do
+    msg = Impl.fetch!(state, todo_list_name)
+    {:reply, msg, state}
+  end
+
+  # defp reply(state, msg) do
+  #   {:reply, msg, state}
+  # end
 end
-  # def handle_call({:server_process, todo_list_name}, _from, state) do
+
   #   state
   #   |> Impl.add_if_doesnt_exist(todo_list_name)
   #   |> reply(Impl.fetch!(state, todo_list_name))
   # end
 
 
-  # defp reply(state, msg) do
-  #   {:reply, msg, state}
-  # end
 # e
