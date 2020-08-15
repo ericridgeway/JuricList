b1e2e9202c89653232ff3edb09041c1797f88d59
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 17:24:29 -0400

    Revert "for revert, not-working pipeline-ification"
    This reverts commit 718a875711426ea5e04dda2bbd7e1e4c5909536b.

--- cache/lib/cache/server.ex
+++ cache/lib/cache/server.ex
@@ -10,27 +10,20 @@ defmodule Cache.Server do
 
   @impl GenServer
   def handle_call({:server_process, todo_list_name}, _from, state) do
-    # state = Impl.add_if_doesnt_exist(state, todo_list_name)
-    # todo_server = Impl.fetch!(state, todo_list_name)
-    state
-    |> Impl.add_if_doesnt_exist(todo_list_name)
-    |> reply_with_process()
-  end
+    state = Impl.add_if_doesnt_exist(state, todo_list_name)
+    todo_server = Impl.fetch!(state, todo_list_name)
 
-  defp reply_with_process(state) do
-    msg = Impl.fetch!(state, todo_list_name)
-    {:reply, msg, state}
+    {:reply, todo_server, state}
   end
-
-  # defp reply(state, msg) do
-  #   {:reply, msg, state}
-  # end
 end
-
+  # def handle_call({:server_process, todo_list_name}, _from, state) do
   #   state
   #   |> Impl.add_if_doesnt_exist(todo_list_name)
   #   |> reply(Impl.fetch!(state, todo_list_name))
   # end
 
 
+  # defp reply(state, msg) do
+  #   {:reply, msg, state}
+  # end
 # e
