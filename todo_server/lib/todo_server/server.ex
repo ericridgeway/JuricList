b8b8fef222ed42851a679f81b82a1e703b0780d3
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 22:05:47 -0400

    TodoServer.Server uses prettier pipelines

--- todo_server/lib/todo_server/server.ex
+++ todo_server/lib/todo_server/server.ex
@@ -8,19 +8,37 @@ defmodule TodoServer.Server do
   end
 
   def handle_cast({:add_entry, entry}, state) do
-    {:noreply, Impl.add_entry(state, entry)}
+    state
+    |> Impl.add_entry(entry)
+    |> noreply()
   end
 
   def handle_cast({:update_entry, id, updater_fun}, state) do
-    {:noreply, Impl.update_entry(state, id, updater_fun)}
+    state
+    |> Impl.update_entry(id, updater_fun)
+    |> noreply()
   end
 
   def handle_cast({:delete_entry, id}, state) do
-    {:noreply, Impl.delete_entry(state, id)}
+    state
+    |> Impl.delete_entry(id)
+    |> noreply()
   end
 
   def handle_call({:entries, date}, _sender, state) do
-    {:reply, Impl.entries(state, date), state}
+    with entries <- Impl.entries(state, date)
+    do
+      state
+      |> reply(entries)
+    end
+  end
+
+
+  defp noreply(state) do
+    {:noreply, state}
+  end
+
+  defp reply(state, msg) do
+    {:reply, msg, state}
   end
 end
-
