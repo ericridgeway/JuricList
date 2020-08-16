606f0849111ef20953eaea3e3c261c832c437cb5
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 21:04:39 -0400

    Logic pulled into seperate Impl file, still skelleton

---
+++ database/lib/database/impl.ex
@@ -0,0 +1,15 @@
+defmodule Database.Impl do
+
+  def new() do
+    -1
+  end
+
+  def store(state, key, data) do
+    -1
+  end
+
+  def get(state, key) do
+    "is batman"
+  end
+end
+
--- database/lib/database/server.ex
+++ database/lib/database/server.ex
@@ -1,19 +1,35 @@
 defmodule Database.Server do
   use GenServer
 
+  alias Database.{Impl}
+
   @impl GenServer
   def init(_init_arg) do
-    {:ok, -1}
+    {:ok, Impl.new()}
   end
 
   @impl GenServer
   def handle_cast({:store, key, data}, state) do
-    {:noreply, state}
+    state
+    |> Impl.store(key, data)
+    |> noreply()
   end
 
   @impl GenServer
   def handle_call({:get, key}, _from, state) do
-    {:reply, "is batman", state}
+    with data <- Impl.get(state, key)
+    do
+      state
+      |> reply(data)
+    end
+  end
+
+
+  defp noreply(state) do
+    {:noreply, state}
+  end
+
+  defp reply(state, msg) do
+    {:reply, msg, state}
   end
 end
-
