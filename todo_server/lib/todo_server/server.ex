149939a22dc8db7270b92237e2639bc45b9d169d
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 22:00:24 -0400

    TodoServer.Server no longer cares what state's internals aare, excellent

--- todo_server/lib/todo_server/server.ex
+++ todo_server/lib/todo_server/server.ex
@@ -1,24 +1,26 @@
 defmodule TodoServer.Server do
   use GenServer
 
+  alias TodoServer.{Impl}
+
   def init(_init_arg) do
-    {:ok, TodoList.new()}
+    {:ok, Impl.new()}
   end
 
-  def handle_cast({:add_entry, entry}, todo_list) do
-    {:noreply, TodoList.add_entry(todo_list, entry)}
+  def handle_cast({:add_entry, entry}, state) do
+    {:noreply, Impl.add_entry(state, entry)}
   end
 
-  def handle_cast({:update_entry, id, updater_fun}, todo_list) do
-    {:noreply, TodoList.update_entry(todo_list, id, updater_fun)}
+  def handle_cast({:update_entry, id, updater_fun}, state) do
+    {:noreply, Impl.update_entry(state, id, updater_fun)}
   end
 
-  def handle_cast({:delete_entry, id}, todo_list) do
-    {:noreply, TodoList.delete_entry(todo_list, id)}
+  def handle_cast({:delete_entry, id}, state) do
+    {:noreply, Impl.delete_entry(state, id)}
   end
 
-  def handle_call({:entries, date}, _sender, todo_list) do
-    {:reply, TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _sender, state) do
+    {:reply, Impl.entries(state, date), state}
   end
 end
 
