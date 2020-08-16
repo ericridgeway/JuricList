8b297f3510514293bd92d54aef07170807854cfd
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-15 20:49:33 -0400

    Skelleton

--- database/lib/database.ex
+++ database/lib/database.ex
@@ -1,3 +1,14 @@
 defmodule Database do
 
+  def start() do
+    {:ok, -1}
+  end
+
+  def store(key, value) do
+    :ok
+  end
+
+  def get(key) do
+    "is batman"
+  end
 end
--- database/test/database_test.exs
+++ database/test/database_test.exs
@@ -1,4 +1,10 @@
 defmodule DatabaseTest do
   use ExUnit.Case
 
+  test "TODO" do
+    {:ok, _database} = Database.start()
+
+    :ok = Database.store(:abed, "is batman")
+    assert Database.get(:abed) == "is batman"
+  end
 end
