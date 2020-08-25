98732f3b4916e2856236048b0b307460ce356719
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-25 15:15:51 -0400

    Kill scaffolding

--- pre8_restart/lib/pre8_restart.ex
+++ pre8_restart/lib/pre8_restart.ex
@@ -1,18 +1,2 @@
 defmodule Pre8Restart do
-  @moduledoc """
-  Documentation for `Pre8Restart`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pre8Restart.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- pre8_restart/test/pre8_restart_test.exs
+++ pre8_restart/test/pre8_restart_test.exs
@@ -1,8 +1,3 @@
 defmodule Pre8RestartTest do
   use ExUnit.Case
-  doctest Pre8Restart
-
-  test "greets the world" do
-    assert Pre8Restart.hello() == :world
-  end
 end
