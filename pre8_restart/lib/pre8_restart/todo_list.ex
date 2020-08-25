9446d747381bad64de94c47c92b62ef0a7f17b04
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-25 15:15:18 -0400

    Partway done basic new/set/get test

--- pre8_restart/lib/pre8_restart/todo_list.ex
+++ pre8_restart/lib/pre8_restart/todo_list.ex
@@ -2,12 +2,21 @@ defmodule Pre8Restart.TodoList do
 
   @type t :: %{id => entry}
   @type id :: pos_integer
-  @type entry :: %{id: id, date: Date, title: String.t}
+  @type entry :: %{id: id, date: Date, title: title}
+  @type title :: String.t
 
   @spec new() :: t
   def new() do
     # %{id: nil, entry: nil}
     # %{cat: :whatever}
     %{}
+  end
+
+  def add_entry(_todo_list, _entry) do
+    -1
+  end
+
+  def titles(_todo_list, _date) do
+    []
   end
 end
--- pre8_restart/test/todo_list_test.exs
+++ pre8_restart/test/todo_list_test.exs
@@ -3,7 +3,14 @@ defmodule Pre8RestartTest.TodoList do
 
   alias Pre8Restart.{TodoList}
 
-  test "new, add_entry, and entries" do
-    _todo_list = TodoList.new()
+  test "new, add_entry, and titles" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert TodoList.titles(todo_list, ~D[3000-01-01]) == []
+    # assert TodoList.titles(~D[2018-01-02]) == ["Dentist", "Meeting"]
   end
 end
