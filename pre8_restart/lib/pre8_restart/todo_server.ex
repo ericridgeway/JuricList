c09a60646734242b6d7baba8236ee38132903341
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-25 21:15:46 -0400

    Save current step partway making genserver wrapper

---
+++ pre8_restart/lib/pre8_restart/todo_server.ex
@@ -0,0 +1,24 @@
+defmodule Pre8Restart.TodoServer do
+  use GenServer
+
+  def start_link() do
+  end
+
+  def add_entry(pid, entry) do
+  end
+
+  def titles(todo_list, target_date) do
+  end
+
+  def entries(todo_list, target_date) do
+  end
+
+  def update_entry(todo_list, id, updater_fun) do
+  end
+
+  def update_entry(todo_list, updated_entry) do
+  end
+
+  def delete_entry(todo_list, id) do
+  end
+end
