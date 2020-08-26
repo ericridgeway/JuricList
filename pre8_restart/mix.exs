bdd42b75dfe134483c569b07537006416704240e
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-25 21:23:27 -0400

    Add mix

--- pre8_restart/mix.exs
+++ pre8_restart/mix.exs
@@ -25,6 +25,7 @@ defmodule Pre8Restart.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:shorter_maps, "~> 2.0"},
+      {:component, "~> 0.2.5"},
     ]
   end
 end
