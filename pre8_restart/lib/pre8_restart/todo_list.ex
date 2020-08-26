b9fa207f7abbfc5dc96ae8cdc9b2f7b986530ca5
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-26 17:36:06 -0400

    Keep original api

--- pre8_restart/lib/pre8_restart/todo_list.ex
+++ pre8_restart/lib/pre8_restart/todo_list.ex
@@ -1,22 +1,27 @@
 defmodule Pre8Restart.TodoList do
-  def start() do
-    GenServer.start(__MODULE__.Server, nil)
+  def new() do
+    {:ok, pid} = GenServer.start(__MODULE__.Server, nil)
+    pid
   end
 
   def add_entry(pid, entry) do
     GenServer.cast(pid, {:add_entry, entry})
+    pid
   end
 
   def update_entry(pid, id, updater_fun) do
     GenServer.cast(pid, {:update_entry, id, updater_fun})
+    pid
   end
 
   def update_entry(pid, updated_entry) do
     GenServer.cast(pid, {:update_entry, updated_entry})
+    pid
   end
 
   def delete_entry(pid, id) do
     GenServer.cast(pid, {:delete_entry, id})
+    pid
   end
 
 
