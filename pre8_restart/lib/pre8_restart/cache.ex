bf2d41c71fe00c6b502242dd9ead8d51862e18ce
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-27 02:06:57 -0400

    Making a new cache "starts" the database

--- pre8_restart/lib/pre8_restart/cache.ex
+++ pre8_restart/lib/pre8_restart/cache.ex
@@ -1,12 +1,18 @@
 defmodule Pre8Restart.Cache do
-  alias Pre8Restart.{TodoList}
+  alias Pre8Restart.{TodoList, Database}
 
   @type t :: %{name => todo_list}
   @type name :: String.t
   @type todo_list :: TodoList.t
 
   @spec new() :: t
-  defdelegate new(), to: Map
+  # defdelegate new(), to: Map
+  def new() do
+    # NOTE tmp hack book asks for, pg 191
+    Database.start()
+
+    %{}
+  end
 
   @spec put_if_doesnt_exist(t, name) :: t
   def put_if_doesnt_exist(state, name) do
