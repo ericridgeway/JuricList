784ed7b0e6f261bcc3427dab50bff55d84a76f38
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-26 21:14:17 -0400

    Refactor database to use file saves instead of map

--- pre8_restart/lib/pre8_restart/database.ex
+++ pre8_restart/lib/pre8_restart/database.ex
@@ -1,20 +1,39 @@
 defmodule Pre8Restart.Database do
-  @type t :: %{}
+  @type t :: :ok
   @type key :: any
   @type value :: any
 
+  @db_folder "./persist"
+
   @spec new() :: t
-  defdelegate new(), to: Map
-  # def new() do
-  #   -1
-  # end
+  # defdelegate new(), to: Map
+  def new() do
+    File.mkdir_p!(@db_folder)
+
+    :ok
+  end
 
   @spec store(t, key, value) :: t
-  defdelegate store(state, key, value), to: Map, as: :put
-  # def store(state, key, value) do
-  #   -1
-  # end
+  # defdelegate store(state, key, value), to: Map, as: :put
+  def store(_state, key, value) do
+    key
+    |> file_name()
+    |> File.write!(:erlang.term_to_binary(value))
+
+    :ok
+  end
 
   @spec get(t, key) :: value
-  defdelegate get(state, key), to: Map
+  # defdelegate get(state, key), to: Map
+  def get(_state, key) do
+    case File.read(file_name(key)) do
+      {:ok, contents} -> :erlang.binary_to_term(contents)
+      _ -> nil
+    end
+  end
+
+
+  defp file_name(key) do
+    Path.join(@db_folder, to_string(key))
+  end
 end
