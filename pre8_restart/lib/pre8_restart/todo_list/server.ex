07ea6dc09f2eb2ad3b5cab1a2d5d553131f37e3b
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-27 02:28:14 -0400

    Refactor todolist.server to end in reply function

--- pre8_restart/lib/pre8_restart/todo_list/server.ex
+++ pre8_restart/lib/pre8_restart/todo_list/server.ex
@@ -10,37 +10,53 @@ defmodule Pre8Restart.TodoList.Server do
 
   @impl GenServer
   def handle_call({:add_entry, entry}, _from, state) do
-    {:reply, :ok, State.add_entry(state, entry)}
+    state
+    |> State.add_entry(entry)
+    |> reply(:ok)
   end
 
   @impl GenServer
   def handle_call({:update_entry, id, updater_fun}, _from, state) do
-    {:reply, :ok, State.update_entry(state, id, updater_fun)}
+    state
+    |> State.update_entry(id, updater_fun)
+    |> reply(:ok)
   end
 
   @impl GenServer
   def handle_call({:update_entry, updated_entry}, _from, state) do
-    {:reply, :ok, State.update_entry(state, updated_entry)}
+    state
+    |> State.update_entry(updated_entry)
+    |> reply(:ok)
   end
 
   @impl GenServer
   def handle_call({:delete_entry, id}, _from, state) do
-    {:reply, :ok, State.delete_entry(state, id)}
+    state
+    |> State.delete_entry(id)
+    |> reply(:ok)
   end
 
 
   @impl GenServer
   def handle_call({:titles, target_date}, _from, state) do
-    {:reply, State.titles(state, target_date), state}
+    state
+    |> reply(State.titles(state, target_date))
   end
 
   @impl GenServer
   def handle_call({:entries, target_date}, _from, state) do
-    {:reply, State.entries(state, target_date), state}
+    state
+    |> reply(State.entries(state, target_date))
   end
 
   @impl GenServer
   def handle_call({:name}, _from, state) do
-    {:reply, State.name(state), state}
+    state
+    |> reply(State.name(state))
+  end
+
+
+  defp reply(state, msg) do
+    {:reply, msg, state}
   end
 end
