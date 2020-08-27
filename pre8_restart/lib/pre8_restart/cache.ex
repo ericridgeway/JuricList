2ed423170b9005ac1dbcc818ec5d59586eb89fe9
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-26 20:02:46 -0400

    Cache partially in place

--- pre8_restart/lib/pre8_restart/cache.ex
+++ pre8_restart/lib/pre8_restart/cache.ex
@@ -1,3 +1,16 @@
 defmodule Pre8Restart.Cache do
+  alias Pre8Restart.{TodoList}
 
+  @type t :: %{}
+  @type name :: String.t
+
+  @spec new() :: t
+  def new() do
+    %{}
+  end
+
+  @spec fetch_list(t, name) :: TodoList.t
+  def fetch_list(state, name) do
+    TodoList.new()
+  end
 end
--- pre8_restart/test/cache_test.exs
+++ pre8_restart/test/cache_test.exs
@@ -2,4 +2,16 @@ defmodule Pre8RestartTest.Cache do
   use ExUnit.Case
 
   alias Pre8Restart.{Cache}
+
+  test "Cache stores unique todoLists under names" do
+    cache =
+      Cache.new()
+
+    bob = Cache.fetch_list(cache, "Bob's list")
+    bob_again = Cache.fetch_list(cache, "Bob's list")
+    alice = Cache.fetch_list(cache, "Alice's list")
+
+    assert bob != alice
+    # assert bob == bob_again
+  end
 end
