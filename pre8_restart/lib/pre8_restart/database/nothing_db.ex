5b75bc7ca3904e60e155b73f898ea27d07faaa9c
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-28 17:47:11 -0400

    Minor todo note

--- pre8_restart/lib/pre8_restart/database/nothing_db.ex
+++ pre8_restart/lib/pre8_restart/database/nothing_db.ex
@@ -12,3 +12,6 @@ defmodule Pre8Restart.Database.NothingDb do
   @impl Database
   def get(_key), do: nil
 end
+
+# ASK @William I wish I could combine this as "defaults" in database.ex
+# TODO actually I bet I can combine this with database.ex, maybe with @optional_callback or something
