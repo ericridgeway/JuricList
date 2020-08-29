30e27f1bd57c885673bb4d63266685b9d91321f5
Eric Ridgeway <ericridgeway@gmail.com>
2020-08-29 01:04:17 -0400

    State keeps map of 3 workers, also add shared typespec in slightly nicer version than the macro thing

--- pre8_restart/lib/pre8_restart/database/file_system_db/state.ex
+++ pre8_restart/lib/pre8_restart/database/file_system_db/state.ex
@@ -1,21 +1,34 @@
 defmodule Pre8Restart.Database.FileSystemDb.State do
   alias Pre8Restart.Database.FileSystemDb.{Worker}
+  alias Pre8Restart.Database, as: D
 
+  @num_workers 3
+
+  @type t :: %{worker_num => worker_pid}
+  @type worker_num :: pos_integer
+  @type worker_pid :: pid
+
+  @spec new() :: t
   def new() do
-    %{worker1: Worker.new()}
+    Enum.reduce(0..@num_workers-1, %{}, fn key, worker_map ->
+      Map.put(worker_map, key, Worker.new())
+    end)
   end
 
+  @spec store(t, D.key, D.value) :: t
   def store(state, key, value) do
-    Worker.store(state.worker1, key, value)
+    Worker.store(state[1], key, value)
     state
   end
 
+  @spec get(t, D.key) :: D.value
   def get(state, key) do
-    Worker.get(state.worker1, key)
+    Worker.get(state[1], key)
   end
 
+  @spec clear(t) :: :ok
   def clear(state) do
-    Worker.clear(state.worker1)
+    Worker.clear(state[1])
   end
 end
 
